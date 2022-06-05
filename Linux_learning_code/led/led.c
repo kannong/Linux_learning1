5309ea18a67d8d8cb042aeaaa017e182fbc593e4
kanong <2637053537@qq.com>
2022-06-06 00:25:35 +0800

    master

--- Linux_learning_code/led/led.c
+++ Linux_learning_code/led/led.c
@@ -1,10 +1,12 @@
 int main()
 {
     //定义方向寄存器以及数据寄存器的地址
-    unsigned int *GPDCON = 0x56000010;
-    unsigned int *GPDDAT = 0x56000014;
+    unsigned int *GPDCON = (unsigned int *)0x56000010;
+    unsigned int *GPDDAT = (unsigned int *)0x56000014;
 
     //写入寄存器
     *GPDCON = 0x00000400;
     *GPDDAT = 0;
+
+    return 0;
 }
