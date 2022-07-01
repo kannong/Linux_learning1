db783e5b9ccb6c5f918bdc936a7eb8d67bcfb31c
kanong <2637053537@qq.com>
2022-07-01 23:28:40 +0800

    master

--- Linux_learning_code/led/led.c
+++ Linux_learning_code/led/led.c
@@ -4,9 +4,9 @@ int main()
     unsigned int *GPDCON = (unsigned int *)0x56000010;
     unsigned int *GPDDAT = (unsigned int *)0x56000014;
 
-    //写入寄存器
-    *GPDCON = 0x00000400;
-    *GPDDAT = 0;
+    //写入寄存器B5
+    *GPDCON = 0x400;
+    *GPDDAT = 0x20;
 
     return 0;
 }
